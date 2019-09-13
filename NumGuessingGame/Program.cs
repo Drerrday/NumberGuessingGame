180833ceb6e9561633a4777e9397142ef8f19f84
Hunter Elam <hunterelam21@gmail.com>
2019-09-13 08:03:34 -0500

    1st edition

---
+++ NumGuessingGame/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace NumGuessingGame
+{
+    class Program
+    {
+        //string question = "Enter the MAX amount of Numbers to Guess from: ";
+
+        static int GetIntegerFromUser(string question)
+        {
+            int integerFromUser;
+            bool success;
+
+            do
+            {
+                Console.WriteLine(question);
+                string userResponse = Console.ReadLine();
+                success = int.TryParse(userResponse, out integerFromUser);
+            } while (success == false);
+
+            return integerFromUser;
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets Play GUESS MY NUMBER!!!");
+            int max = GetIntegerFromUser("What max range would you like: ");
+            Random rnd = new Random();
+
+            int secretNumber = rnd.Next(1, max + 1); //FIND THIS NUMBER
+            int close = 0;
+
+            int i = 1;
+
+            do
+            {
+                int guessNum = GetIntegerFromUser("Guess a number: ");
+                if (guessNum < secretNumber)
+                {
+                    Console.WriteLine("WRONG, Guess a Higher Number: ");
+                    i += 1;
+                }
+                else if (guessNum > secretNumber)
+                {
+                    Console.WriteLine("WRONG, Guess a Lower Number: ");
+                    i += 1;
+                }
+                else if (guessNum == secretNumber)
+                {
+                    Console.WriteLine($"You Win!!! it only took {i} number of guesses!");
+                    close = 1;
+                }
+            } while (close != 1);
+
+        }
+    }
+}
